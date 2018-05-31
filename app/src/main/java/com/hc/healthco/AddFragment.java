8edb5460499cd50066a5dd3c421067872ee8005a
Alyssa Nguyen <halongpbn@gmail.com>
2018-05-31 08:28:35 -0700

    Update AddFragment.java

--- app/src/main/java/com/hc/healthco/AddFragment.java
+++ app/src/main/java/com/hc/healthco/AddFragment.java
@@ -1,19 +1,38 @@
 package com.hc.healthco;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.hc.healthco.AccountActivity.LoginActivity;
 import com.hc.healthco.R;
 
 public class AddFragment extends Fragment {
+    private FloatingActionButton add;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+    @Override
+    public void onCreate(Bundle savedInstances)
+    {
+        super.onCreate(savedInstances);
+        FragmentActivity context = getActivity();
+        add = (FloatingActionButton) context.findViewById(R.id.adding);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), CameraActivity.class));
+            }
+        });
     }
 }
